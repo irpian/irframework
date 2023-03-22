ea8a4aa4a793fda382058f4ac6f786595076c700
Bang Irpie <bang.irpie@gmail.com>
2023-03-23 06:19:42 +0700

    update route

--- app/route.php
+++ app/route.php
@@ -1,16 +1,44 @@
 <?php
-
-if($is_backend){
-	if(in_array($alias, $list_module)){
-		include "modules/".$alias."/backend/route.php";
-	}else{
-		include "modules/home/backend/route.php";
+class Route(){
+	
+	function state(){
+		global $config;
+		if($param[1] == "admin"){
+			$state = "backend";
+		} else {
+			$state = "frontend";
+		}
+		
+		return $state;
 	}
-} else {
-	if(in_array($alias, $list_module)){
-		include "modules/".$alias."/route.php";
-	}else{
-		include "modules/home/route.php";
+	
+	function alias(){
+		
+	}
+	
+	function last(){
+		
+	}
+	
+	function set(){
+		global $config;
+		if($this->state() == "backend"){
+			if(in_array($alias, $list_module)){
+				include "modules/".$alias."/backend/route.php";
+			}else{
+				include "modules/home/backend/route.php";
+			}
+		} else {
+			if(in_array($alias, $list_module)){
+				include "modules/".$alias."/route.php";
+			}else{
+				include "modules/home/route.php";
+			}
+		}
 	}
 }
+
+
+$route = new Route():
+$route->set();
 ?>
